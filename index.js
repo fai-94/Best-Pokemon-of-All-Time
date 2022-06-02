eb6ebc2d51edc4f854715d8dcf22629fb0a3550e
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-06-02 17:33:10 -0400

    revise index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+baseUrl = 'http://localhost:3000/pokemon'
 
 fetch(`${baseUrl}`)
     .then(response => response.json())
     .then(data => {
-        renderPokemon(data[0])
+        renderPokemon(data[1])
         renderPokemonLists(data)
     })
 
-// function renderPokemon(data) {
-//   const pokemonSelection = document.getElementById('')
-//     // pokemonSelection = 
-// }
+    //get first Pokemon details
+function renderPokemon(pokemon) {
+    const pokemonName = document.getElementById('pokemon-name')
+        pokemonName.textContent = pokemon.name
+    const pokemonHp = document.getElementById('pokemon-hp')
+        pokemonHp.textContent = pokemon.hp
+    const pokemonImage = document.getElementById('pokemon-image')
+         pokemonImage.src = pokemon.sprites.front        
+}   
+
+function renderPokemonLists(pokemon) {
+    // console.log(pokemon[0])
+    pokemon.forEach(object => {
+        const pokemonPic = document.createElement('img')
+            pokemonPic.src = object.sprites.front
+         const grabDiv = document.getElementById('pokemon-pic')
+         pokemonPic.addEventListener('click', () => renderPokemon(object))
+         
+        grabDiv.append(pokemonPic)
+    } 
+    )
+}
